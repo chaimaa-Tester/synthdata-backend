ac0c473bd6c27e805549dffd3b16492a65177d4f
Jan Krämer <jan.kraemer04@web.de>
2025-11-14 16:11:41 +0100

    letzte Anpassungen an main branch

--- Backend/generators/logistic.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-def generate_logisticData(rows, rowCount):
-    data = [{"Container": "C123", "Status": "Versendet", "Ort": "Hamburg"} for _ in range(rowCount)]
-    return pd.DataFrame(data)
